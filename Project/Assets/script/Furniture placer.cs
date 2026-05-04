23470e5484c9d5bd599d2dfe2d16953d9991b628
Asleepdoggy <maomaoovo@outlook.com>
2026-05-04 14:30:17 +0800

    feat: 增加输入兼容，方便在电脑上运行项目

--- Project/Assets/script/Furniture placer.cs
+++ Project/Assets/script/Furniture placer.cs
@@ -15,11 +15,41 @@ public class FurniturePlacer : MonoBehaviour
 
     void Update()
     {
-        // 安卓触摸点击
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        #region 输入兼容处理（方便在电脑上运行）
+
+        bool b_IsInputDetected = false;
+        Vector2 inputPos = Vector2.zero;
+
+        //如果在编辑器中运行，输入是来源于鼠标点击
+        if (Application.isEditor)
         {
-            if (raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.Planes))
+            b_IsInputDetected = Input.GetMouseButtonDown(0);
+            inputPos = Input.mousePosition;
+        }
+        else //如果不是，输入是来源于触屏
+        {
+            b_IsInputDetected = Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began;
+            if(b_IsInputDetected) inputPos = Input.GetTouch(0).position;
+        }
+
+        #endregion
+        
+        //检测到输入
+        if (b_IsInputDetected)
+        {
+            Debug.Log("检测到输入！");
+            
+            //射线模块检查
+            if (raycastManager.subsystem == null)
             {
+                Debug.LogError("射线检测子系统未运行！请检查 XR Plug-in Management 设置。");
+                return;
+            }
+            
+            if (raycastManager.Raycast(inputPos, hits, TrackableType.Planes))
+            {
+                Debug.Log("点中了平面！");
+                
                 Pose hitPose = hits[0].pose;
 
                 // 如果没有家具，生成新家具
@@ -33,6 +63,10 @@ public class FurniturePlacer : MonoBehaviour
                 {
                     spawnedFurniture.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
                 }
+            }
+            else
+            {
+                Debug.LogWarning("射线发射了，但没点中任何平面。当前平面数量：" + hits.Count);
             }
         }
     }
